5b211f1ed07d098597c8c6a6b8554647d66ba59c
Abhishek Sharma <abhisheksharma172005@gmail.com>
2026-02-03 20:26:25 +0530

    fix template filename case

--- reconcilar.py
+++ reconcilar.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("Reconcilar_index.html")
+    return render_template("reconcilar_index.html")
 
 if __name__ == "__main__":
     app.run()
